e31cf65292906fe500a92165b1c338d1cbc9b840
Pedro <pfdiaz2015@gmail.com>
2025-07-24 09:30:05 -0300

    Cambios en introduction

--- Introduction/receiver.py
+++ Introduction/receiver.py
@@ -1,25 +1,29 @@
-#!/usr/bin/env python
 import pika, sys, os
+from pika.adapters.blocking_connection import BlockingChannel
+from pika import BlockingConnection
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+    connection: BlockingConnection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel: BlockingChannel = connection.channel()
 
     channel.queue_declare(queue='hello')
 
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+        logging.info(f" [x] Received {body}")
 
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    logging.info(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        logging.info('Interrupted')
         try:
             sys.exit(0)
         except SystemExit:
--- Introduction/send.py
+++ Introduction/send.py
@@ -1,8 +1,16 @@
 import pika
 from pika import BlockingConnection
+from pika.adapters.blocking_connection import BlockingChannel
+import logging
+
+# This script sends a message to a RabbitMQ queue named 'hello'.
+# It uses the pika library to connect to RabbitMQ, declare the queue, and publish a message.
+# It also sets up basic logging to track the message sending process.
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 connection: BlockingConnection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+channel: BlockingChannel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
@@ -10,6 +18,6 @@ channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
 
-print(" [x] Sent 'Hello World!'")
+logging.info(" [x] Sent 'Hello World!'")
 
 connection.close()
